d7a2fdb8c092a4f67da87ea12568f85c61a18162
Colby Pham <61342696+NopeNotDark@users.noreply.github.com>
2023-06-10 20:04:09 -0400

    Impl config for shoot-throughable blocks

--- src/nopenotdark/spyglass_sniper/Snipers.php
+++ src/nopenotdark/spyglass_sniper/Snipers.php
@@ -1,22 +1,45 @@
 <?php
 
 /**
- * Written by PocketAI (A revolutionary AI for PocketMine-MP plugin developing)
+ * `7MM"""Mq.                 `7MM              mm        db     `7MMF'
+ *   MM   `MM.                  MM              MM       ;MM:      MM
+ *   MM   ,M9 ,pW"Wq.   ,p6"bo  MM  ,MP.gP"Ya mmMMmm    ,V^MM.     MM
+ *   MMmmdM9 6W'   `Wb 6M'  OO  MM ;Y ,M'   Yb  MM     ,M  `MM     MM
+ *   MM      8M     M8 8M       MM;Mm 8M""""""  MM     AbmmmqMA    MM
+ *   MM      YA.   ,A9 YM.    , MM `MbYM.    ,  MM    A'     VML   MM
+ * .JMML.     `Ybmd9'   YMbmd'.JMML. YA`Mbmmd'  `Mbm.AMA.   .AMMA.JMML.
  *
+ * This file was generated using PocketAI, Branch Stable, V6.20.1
+ *
+ * PocketAI is private software: You can redistribute the files under
+ * the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This plugin is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this file.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @ai-profile: NopeNotDark
  * @copyright 2023
+ * @authors NopeNotDark, SantanasWrld
+ * @link https://thedarkproject.net/pocketai
  *
- * This file was refactored by PocketAI (A revolutionary AI for PocketMine-MP plugin developing)
  */
 
 namespace nopenotdark\spyglass_sniper;
 
 use NhanAZ\libRegRsp\libRegRsp;
 use pocketmine\color\Color;
-use pocketmine\entity\Entity;
 use pocketmine\entity\Living;
 use pocketmine\entity\Location;
 use pocketmine\entity\projectile\Snowball;
 use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\item\StringToItemParser;
 use pocketmine\math\AxisAlignedBB;
 use pocketmine\math\Vector3;
 use pocketmine\network\mcpe\protocol\PlaySoundPacket;
@@ -27,13 +50,20 @@ use pocketmine\utils\TextFormat as C;
 use pocketmine\world\particle\DustParticle;
 
 final class Snipers extends PluginBase {
+
     use SingletonTrait;
 
-    public function onEnable() : void {
+    private array $shootableBlocks;
+
+    public function onEnable(): void {
         self::setInstance($this);
         libRegRsp::regRsp($this);
         $this->saveDefaultConfig();
         $this->getServer()->getPluginManager()->registerEvents(new SnipersListener(), $this);
+
+        foreach ($this->getConfig()->get("shootable-blocks", []) as $blockName) {
+            $this->shootableBlocks[] = StringToItemParser::getInstance()->parse($blockName)->getTypeId();
+        }
     }
 
     public function handleSpyglassAnimation(Player $player): void {
@@ -64,6 +94,12 @@ final class Snipers extends PluginBase {
     }
 
     public function handleEntityDamage(Player $player, Vector3 $dustPosition): void {
+        $blockAt = $player->getWorld()->getBlockAt($dustPosition->getX(), $dustPosition->getY(), $dustPosition->getZ());
+
+        if (!in_array($blockAt->getTypeId(), $this->shootableBlocks)) {
+            return;
+        }
+
         $boundingBox = new AxisAlignedBB(
             $dustPosition->getX() - 0.2,
             $dustPosition->getY() - 0.2,
@@ -78,49 +114,47 @@ final class Snipers extends PluginBase {
         $damageMultiplier = 1;
         $playerAttacked = false;
 
-        if (!empty($entities)) {
-            foreach ($entities as $entity) {
-                if (!$entity instanceof Living) {
-                    continue;
-                }
+        foreach ($entities as $entity) {
+            if (!$entity instanceof Living) {
+                continue;
+            }
 
-                $entityHeight = $entity->getSize()->getHeight();
-                $hitDistance = $dustPosition->distance($entity->getLocation());
-                if ($hitDistance <= $entityHeight) {
-                    $entityHeadY = $entity->getLocation()->getY() + $entityHeight - $entity->getEyeHeight();
+            $entityHeight = $entity->getSize()->getHeight();
+            $hitDistance = $dustPosition->distance($entity->getLocation());
 
-                    if ($dustPosition->getY() >= $entityHeadY) {
-                        // Loop sends message to player, loop ends, but still sends no clue why ???
-                        // $player->sendMessage(C::GREEN . "+" . C::DARK_GREEN . "1" . C::GREEN . " Headshot");
-                        $event = new EntityDamageEvent($player, EntityDamageEvent::CAUSE_ENTITY_ATTACK, 21 * $damageMultiplier);
-                    } else {
-                        // Loop sends message to player, loop ends, but still sends no clue why ???
-                        // $player->sendMessage(C::GREEN . "+" . C::DARK_GREEN . "1" . C::YELLOW . " Bodyshot");
-                        $event = new EntityDamageEvent($player, EntityDamageEvent::CAUSE_ENTITY_ATTACK, 20 / 5 * $damageMultiplier);
-                    }
+            if ($hitDistance <= $entityHeight) {
+                $entityHeadY = $entity->getLocation()->getY() + $entityHeight - $entity->getEyeHeight();
+
+                if ($dustPosition->getY() >= $entityHeadY) {
+                    // Headshot
+                    $event = new EntityDamageEvent($player, EntityDamageEvent::CAUSE_ENTITY_ATTACK, 21 * $damageMultiplier);
                 } else {
-                    // Missed Shot
-                    continue;
+                    // Bodyshot
+                    $event = new EntityDamageEvent($player, EntityDamageEvent::CAUSE_ENTITY_ATTACK, 20 / 5 * $damageMultiplier);
                 }
+            } else {
+                // Missed Shot
+                continue;
+            }
 
-                if ($player === $entity) {
-                    if ($this->getConfig()->get("backfire")) {
-                        if ($this->getBackfireBool()) {
-                            $player->sendMessage(C::RED . "Your gun malfunctioned and your hand was blown off.");
-                            $entity->attack($event);
-                            $playerAttacked = true;
-                            break;
-                        }
+            if ($player === $entity) {
+                if ($this->getConfig()->get("backfire")) {
+                    if ($this->getBackfireBool()) {
+                        $player->sendMessage(C::RED . "Your gun malfunctioned and your hand was blown off.");
+                        $entity->attack($event);
+                        $playerAttacked = true;
+                        break;
                     }
-                    $playerAttacked = true;
-                    break;
                 }
+                $playerAttacked = true;
+                break;
+            }
 
-                $entity->attack($event);
-                $damageMultiplier -= 0.2;
-                if ($damageMultiplier < 0.2) {
-                    break;
-                }
+            $entity->attack($event);
+            $damageMultiplier -= 0.2;
+
+            if ($damageMultiplier < 0.2) {
+                break;
             }
         }
 
@@ -132,7 +166,8 @@ final class Snipers extends PluginBase {
     public function getBackfireBool(): bool {
         $first = mt_rand(0, 300);
         $second = mt_rand(0, 300);
-        if ($first === $second) {
+
+        if ($first == $second) {
             return true;
         }
         return false;
@@ -154,8 +189,7 @@ final class Snipers extends PluginBase {
         $x = -sin($yaw / 180 * M_PI) * cos($pitch / 180 * M_PI);
         $y = -sin($pitch / 180 * M_PI);
         $z = cos($yaw / 180 * M_PI) * cos($pitch / 180 * M_PI);
+
         return new Vector3($x, $y, $z);
     }
-
-
 }
--- src/nopenotdark/spyglass_sniper/SnipersListener.php
+++ src/nopenotdark/spyglass_sniper/SnipersListener.php
@@ -1,11 +1,34 @@
 <?php
 
 /**
- * Written by PocketAI (A revolutionary AI for PocketMine-MP plugin developing)
+ * `7MM"""Mq.                 `7MM              mm        db     `7MMF'
+ *   MM   `MM.                  MM              MM       ;MM:      MM
+ *   MM   ,M9 ,pW"Wq.   ,p6"bo  MM  ,MP.gP"Ya mmMMmm    ,V^MM.     MM
+ *   MMmmdM9 6W'   `Wb 6M'  OO  MM ;Y ,M'   Yb  MM     ,M  `MM     MM
+ *   MM      8M     M8 8M       MM;Mm 8M""""""  MM     AbmmmqMA    MM
+ *   MM      YA.   ,A9 YM.    , MM `MbYM.    ,  MM    A'     VML   MM
+ * .JMML.     `Ybmd9'   YMbmd'.JMML. YA`Mbmmd'  `Mbm.AMA.   .AMMA.JMML.
  *
+ * This file was generated using PocketAI, Branch Stable, V6.20.1
+ *
+ * PocketAI is private software: You can redistribute the files under
+ * the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This plugin is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this file.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @ai-profile: NopeNotDark
  * @copyright 2023
+ * @authors NopeNotDark, SantanasWrld
+ * @link https://thedarkproject.net/pocketai
  *
- * This file was refactored by PocketAI (A revolutionary AI for PocketMine-MP plugin developing)
  */
 
 namespace nopenotdark\spyglass_sniper;
@@ -19,6 +42,7 @@ use pocketmine\item\ItemTypeIds;
 use pocketmine\item\VanillaItems;
 use pocketmine\network\mcpe\protocol\AnimatePacket;
 use pocketmine\player\Player;
+use pocketmine\plugin\PluginOwnedTrait;
 use pocketmine\utils\TextFormat as C;
 
 final class SnipersListener implements Listener {
@@ -36,7 +60,7 @@ final class SnipersListener implements Listener {
 
                 $currentTime = time();
                 $lastUsageTime = $this->getLastUsageTime($cooldownKey);
-                $cooldownDuration = Snipers::getInstance()->getConfig()->get("ammo-cooldown", 4);
+                $cooldownDuration = $this->getOwningPlugin()->getConfig()->get("ammo-cooldown", 4);
 
                 if ($currentTime - $lastUsageTime < $cooldownDuration) {
                     $remainingCooldown = $cooldownDuration - ($currentTime - $lastUsageTime);
@@ -56,7 +80,7 @@ final class SnipersListener implements Listener {
                     break;
                 }
 
-                Snipers::getInstance()->handleSpyglassAnimation($player);
+                $this->getOwningPlugin()->handleSpyglassAnimation($player);
 
                 $this->updateLastUsageTime($cooldownKey);
             }
@@ -67,7 +91,7 @@ final class SnipersListener implements Listener {
         $player = $event->getPlayer();
         $cause = $player->getLastDamageCause();
 
-        if (!Snipers::getInstance()->getConfig()->get("custom-death-messages", true)) {
+        if (!$this->getOwningPlugin()->getConfig()->get("custom-death-messages", true)) {
             return;
         }
         $deathMessage = C::RED . $player->getName() . " ";
@@ -89,6 +113,10 @@ final class SnipersListener implements Listener {
                 break;
         }
         $event->setDeathMessage($deathMessage);
+    }
+
+    public function getOwningPlugin(): Snipers {
+        return Snipers::getInstance();
     }
 
 }
